651b847d286e2783274557f9dece036b8c2cad56
lucasaguiar-la <lucas.aguiar.ads@gmail.com>
2024-04-27 20:30:37 -0300

    test: Character class

---
+++ src/rpg/Characters/Character.java
@@ -0,0 +1,13 @@
+package rpg.Characters;
+
+public class Character {
+
+	int heathPoint, manaPoints;
+	int strength, dexterity, defense;
+	int level, currentExperience, nextLevelExperience;
+	
+	void attack() {
+		System.out.println("Attacked!");
+	}
+	
+}
